16caa15b7e0e31f186e09142a1afde0f0a8c09da
Stefan Gelhorn <stefan.gelhorn@googlemail.com>
2025-04-18 16:23:16 +0200

    Bilder für Produkte eingefügt

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,18 +2,24 @@ import React, { Component } from 'react';
 import Navbar from './navbar';
 import Product from './product';
 
+let products = ["Apple", "Banana", "Lemons", "Strawberry", "Plums"];
+let imageSources = ["/assets/img/apple.jpg", "/assets/img/banana.jpg", "/assets/img/lemons.jpg", "/assets/img/strawberries.jpg", "/assets/img/plums.jpg"];
+
+
 class App extends Component {
     state = {  } 
     render() { 
-        return <>
-            <Navbar/>
+        return (
+            <>
+            <Navbar />
             <div className = "product-container">
-                <Product/>
-                <Product/>
-                <Product/>
-                <Product/>
+            {products.map((product, idx) => (
+                <Product key={idx} title={product} source={imageSources[idx]}/>
+            ))}
+
             </div>
             </> 
+        );
     }
 }
  
