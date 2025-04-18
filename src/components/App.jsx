656f403180735c93e03f9ec1dddcaa692ce74cf4
Stefan Gelhorn <stefan.gelhorn@googlemail.com>
2025-04-18 19:07:37 +0200

    Produkte individualisiert

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,10 +8,10 @@ class App extends Component {
         return <>
             <Navbar/>
             <div className = "product-container">
-                <Product/>
-                <Product/>
-                <Product/>
-                <Product/>
+                <Product image="apple.jpg" title="Apples" description="Saftig, süß und ein echter Klassiker. Unsere Äpfel sind die perfekte Balance aus Frische und Geschmack - ideal für jeden Tag." />
+                <Product image="cucumber.jpg" title="Cucumbers" description="Frisch, knackig und erfrischend - unsere Gurken sind perfekt für Salate, Smoothies oder einfach als gesunder Snack zwischendurch." />
+                <Product image="tomatoe.jpg" title="Tomatoes" description="Sonnengereift und voller Geschmack! Diese Tomaten bringen Farbe und Aroma auf deinen Teller - egal ob roh, gekocht oder gegrillt."/>
+                <Product image="pear.jpg" title="Pears" description="Zart, aromatisch und leicht süß - Birnen sind nicht nur lecker, sondern auch vollgepackt mit wertvollen Nährstoffen."/>
             </div>
             </> 
     }
--- src/components/product.jsx
+++ src/components/product.jsx
@@ -5,14 +5,15 @@ import React, { Component } from 'react';
 class Product extends Component {
     state = {  } 
     render() { 
+        const {image, title, description} = this.props;
         return <div className="card" styles={{width: '18rem'}}>
-        <img src="/assets/img/vegetables.jpg" className="card-img-top" alt="..."/>
+        <img src={"/assets/img/" + image} className="card-img-top" alt="..."/>
         <div className="card-body">
-          <h5 className="card-title">Card title</h5>
-          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href="#" className="btn btn-primary">Go somewhere</a>
+          <h5 className="card-title">{title}</h5>
+          <p className="card-text">{description}</p>
+          <a href="/" className="btn btn-primary">Hinzufügen</a>
         </div>
-      </div>;
+      </div>
     }
 }
  
